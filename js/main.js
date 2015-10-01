b6b0906223125555aae61f5180a7bc1c9e8f59e5
Zachary Schuessler <zlschuessler@gmail.com>
2015-10-01 13:06:45 -0700

    many minor UX updates for design/layouts

--- js/main.js
+++ js/main.js
@@ -1,23 +1,35 @@
 var elements = document.getElementsByTagName('script')
 
-Array.prototype.forEach.call(elements, function(element) {
-  if (element.type.indexOf('math/tex') != -1) {
-     // Extract math markdown
-     var textToRender = element.innerText || element.textContent;
+Array.prototype.forEach.call(elements, function (element) {
+    if (element.type.indexOf('math/tex') != -1) {
+        // Extract math markdown
+        var textToRender = element.innerText || element.textContent;
 
-     // Create span for KaTeX
-     var katexElement = document.createElement('span');
+        // Create span for KaTeX
+        var katexElement = document.createElement('span');
 
-     // Support inline and display math
-     if (element.type.indexOf('mode=display') != -1){
-       katexElement.className += "math-display";
-       textToRender = '\\displaystyle {' + textToRender + '}';
-     } else {
-       katexElement.className += "math-inline";
-     }
+        // Support inline and display math
+        if (element.type.indexOf('mode=display') != -1) {
+            katexElement.className += "math-display";
+            textToRender = '\\displaystyle {' + textToRender + '}';
+        } else {
+            katexElement.className += "math-inline";
+        }
 
-     katex.render(textToRender, katexElement);
-     element.parentNode.insertBefore(katexElement, element);
-  }
-  
+        katex.render(textToRender, katexElement);
+        element.parentNode.insertBefore(katexElement, element);
+    }
+
+});
+
+$(document).ready(function() {
+    var currentUrl = window.location.href;
+
+    if (-1 !== currentUrl.indexOf('/learn')) {
+        $('.page-link_learn').addClass('nav-active');
+    } else if (-1 !== currentUrl.indexOf('/demo')) {
+        $('.page-link_demos').addClass('nav-active');
+    } else {
+        $('.page-link_home').addClass('nav-active');
+    }
 });
